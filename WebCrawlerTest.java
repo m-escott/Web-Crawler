3f3106efc251dc61c214cc1c8a7731e27849d498
m-escott <69815692+m-escott@users.noreply.github.com>
2020-10-30 17:14:59 -0400

    Add files via upload

---
+++ WebCrawlerTest.java
@@ -0,0 +1,56 @@
+package com.taryaganalytics.tests.parsers;
+
+import com.taryaganalytics.configs.BHS;
+import com.taryaganalytics.configs.BaseHaSeferConfig;
+import com.taryaganalytics.configs.TF;
+import com.taryaganalytics.dao.DAOFactory;
+import com.taryaganalytics.parsers.WebCrawler;
+import com.taryaganalytics.tests.BaseTest;
+import com.taryaganalytics.util.MorphUtils;
+import com.taryaganalytics.util.ProcessUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.validation.constraints.AssertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+@ContextConfiguration(classes = {BaseHaSeferConfig.class})
+public class WebCrawlerTest extends BaseTest {
+    protected static String TEMPFILES;
+
+    @Before
+    public void setUp() throws Exception {
+        BaseHaSeferConfig config = new BaseHaSeferConfig();
+        TEMPFILES = config.TEMPFILES();
+        setAutowireTargetClasses(new ArrayList(Arrays.asList(DAOFactory.class, TF.class, BHS.class, MorphUtils.class, ProcessUtils.class)));
+        super.setUp();
+    }
+
+    @Test
+    public void testWebCrawler() throws Exception {
+        WebCrawler crawler = new WebCrawler();
+        Map<String, ArrayList<String>> results;
+        results = crawler.crawl("https://wiprodigital.com", "c://builditTest//siteMapResults.txt", 0, 5);
+        assertNotNull(results);
+        assertTrue(results.size() > 0);
+        assertTrue(results.containsKey("https://wiprodigital.com"));
+        System.out.println("all done.");
+
+/*
+Readme.MD
+This was written in Java with the class WebCrawler and the JUnit test class WebCrawlerTest
+For now I have submitted the code and I can put the build content in later
+
+to run this - use JUnit : run or debug the method com.taryaganalytics.tests.parsers.WebCrawlerTest.testWebCrawler()
+
+unit tests
+explain threading issue
+explain robot.txt issue
+extra comments and Javadoc
+ */
+    }
+
+}
