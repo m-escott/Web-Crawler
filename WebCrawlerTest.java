34b04268540e3d31ca2d0a9080bac4be5acdb90e
m-escott <69815692+m-escott@users.noreply.github.com>
2020-11-02 11:22:59 -0500

    Add files via upload

--- WebCrawlerTest.java
+++ WebCrawlerTest.java
@@ -1,39 +1,21 @@
 package com.taryaganalytics.tests.parsers;
 
-import com.taryaganalytics.configs.BHS;
-import com.taryaganalytics.configs.BaseHaSeferConfig;
-import com.taryaganalytics.configs.TF;
-import com.taryaganalytics.dao.DAOFactory;
 import com.taryaganalytics.parsers.WebCrawler;
-import com.taryaganalytics.tests.BaseTest;
-import com.taryaganalytics.util.MorphUtils;
-import com.taryaganalytics.util.ProcessUtils;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
 
-import javax.validation.constraints.AssertTrue;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Map;
 
-@ContextConfiguration(classes = {BaseHaSeferConfig.class})
-public class WebCrawlerTest extends BaseTest {
-    protected static String TEMPFILES;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-    @Before
-    public void setUp() throws Exception {
-        BaseHaSeferConfig config = new BaseHaSeferConfig();
-        TEMPFILES = config.TEMPFILES();
-        setAutowireTargetClasses(new ArrayList(Arrays.asList(DAOFactory.class, TF.class, BHS.class, MorphUtils.class, ProcessUtils.class)));
-        super.setUp();
-    }
+public class WebCrawlerTest {
 
     @Test
     public void testWebCrawler() throws Exception {
         WebCrawler crawler = new WebCrawler();
         Map<String, ArrayList<String>> results;
-        results = crawler.crawl("https://wiprodigital.com", "c://builditTest//siteMapResults.txt", 0, 5);
+        results = crawler.crawl("https://wiprodigital.com", "siteMapResults.txt", 5);
         assertNotNull(results);
         assertTrue(results.size() > 0);
         assertTrue(results.containsKey("https://wiprodigital.com"));
